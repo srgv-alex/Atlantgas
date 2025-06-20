7712628c45cd1c4cc48073f8465298e39e459b07
unknown <jarulcrip@gmail.com>
2025-06-20 17:14:19 +0300

    add advantages and gas-order

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-// FancyBox
+const items = document.querySelectorAll('.content-item');
+
+items.forEach(item => {
+	item.addEventListener('click', () => {
+		items.forEach(i => i.classList.remove('active'));
+		item.classList.add('active');
+	});
+});
