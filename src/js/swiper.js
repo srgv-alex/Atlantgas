9d13ee9f0f161d51a1999389e1a7085ccb1a627e
unknown <jarulcrip@gmail.com>
2025-06-24 20:14:29 +0300

    ready reviews

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,16 +1,6 @@
 const swiper = new Swiper('.swiper', {
-	// effect: 'coverflow',
+	effect: 'coverflow',
 	grabCursor: true,
-	centeredSlides: true,
-	// slidesPerView: 'auto', // чтобы задать ширину в CSS
-	// spaceBetween: 40,
-	// coverflowEffect: {
-	// 	rotate: 0,
-	// 	stretch: 0,
-	// 	depth: 150,
-	// 	modifier: 2,
-	// 	slideShadows: false,
-	// },
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
